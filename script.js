6bc3175a73f8d218f9b4c53e2ecdf84e4d412d98
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-05-22 22:07:03 -0400

    redesign step 1

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-var c_modal = document.getElementById("myContactModal");
-var c_btn = document.getElementById("myContactBtn");
-var c_span = document.getElementsByClassName("c_close")[0];
-c_btn.onclick = function() { c_modal.style.display = "block"; }
-c_span.onclick = function() { c_modal.style.display = "none"; }
+$('#myContactBtn').on('click', function() { $('#myContactModal').css('display', 'block'); });
+$('.c_close').first().on('click', function() { $('#myContactModal').css('display', 'none'); });
 
-var ep_modal = document.getElementById("myEPModal");
-var ep_btn = document.getElementById("myEPBtn");
-var ep_span = document.getElementsByClassName("ep_close")[0];
-ep_btn.onclick = function() { ep_modal.style.display = "block"; }
-ep_span.onclick = function() { ep_modal.style.display = "none"; }
+$('#myEPBtn').on('click', function() { $('#myEPModal').css('display', 'block'); });
+$('.ep_close').first().on('click', function() { $('#myEPModal').css('display', 'none'); });
 
-var f_modal = document.getElementById("myFModal");
-var f_btn = document.getElementById("myFBtn");
-var f_span = document.getElementsByClassName("f_close")[0];
-f_btn.onclick = function() { f_modal.style.display = "block"; }
-f_span.onclick = function() { f_modal.style.display = "none"; }
+$('#myFBtn').on('click', function() { $('#myFModal').css('display', 'block'); });
+$('.f_close').first().on('click', function() { $('#myFModal').css('display', 'none'); });
 
-var art_modal = document.getElementById("myArtModal");
-var art_btn = document.getElementById("myArtBtn");
-var art_span = document.getElementsByClassName("art_close")[0];
-art_btn.onclick = function() { art_modal.style.display = "block"; }
-art_span.onclick = function() { art_modal.style.display = "none"; }
+$('#myArtBtn').on('click', function() { $('#myArtModal').css('display', 'block'); });
+$('.art_close').first().on('click', function() { $('#myArtModal').css('display', 'none'); });
