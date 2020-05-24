7b280bf68bcde837a342cf78a766ed2beb5b44ea
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-05-24 00:52:37 -0400

    intro!

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ $('.f_close').first().on('click', function() { $('#myFModal').css('display', 'no
 
 $('#myArtBtn').on('click', function() { $('#myArtModal').css('display', 'block'); });
 $('.art_close').first().on('click', function() { $('#myArtModal').css('display', 'none'); });
+
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+$(async function() {
+  var l = ['peaceful', 'grateful', 'truthful', 'thoughtful', 'mindful', 'beautiful', 'powerful', 'GIA'];
+  var r = ['innovator', 'creator', 'friend', 'thinker', 'writer', 'citizen', 'ideator', 'GRIER'];
+  var i = 0;
+
+  while (true) {
+    i = i % 8;
+    await sleep(2000);
+    $('#gia').html(l[i]);
+    await sleep(2000);
+    $('#grier').html(r[i]);
+    i++;
+  }
+})
