c0e2d6a9420487399dae0c5a5c8cd7ab1a6ce0bb
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-06-02 14:50:26 -0400

    brand-new modal back-end, baybee

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-$('#myContactBtn').on('click', function() { $('#myContactModal').css('display', 'block'); });
-$('#c_close').on('click', function() { $('#myContactModal').css('display', 'none'); });
-
-$('#myPBtn').on('click', function() { $('#myPModal').css('display', 'block'); });
-$('#p_close').on('click', function() { $('#myPModal').css('display', 'none'); });
-
-$('#myFBtn').on('click', function() { $('#myFModal').css('display', 'block'); });
-$('#f_close').on('click', function() { $('#myFModal').css('display', 'none'); });
-
-$('#myArtBtn').on('click', function() { $('#myArtModal').css('display', 'block'); });
-$('#art_close').on('click', function() { $('#myArtModal').css('display', 'none'); });
-
-$('#myCampBtn').on('click', function() { $('#myCampModal').css('display', 'block'); });
-$('#camp_close').on('click', function() { $('#myCampModal').css('display', 'none'); });
-
-$('#myEPBtn').on('click', function() { $('#myEPModal').css('display', 'block'); });
-$('#ep_close').on('click', function() { $('#myEPModal').css('display', 'none'); });
-
-$('#mySisBtn').on('click', function() { $('#mySisModal').css('display', 'block'); });
-$('#sis_close').on('click', function() { $('#mySisModal').css('display', 'none'); });
+function show(id) { $('#' + id.substr(0, id.length - 3) + 'Modal').css('display', 'block'); }
+function hide(id) { $('#' + id.substr(0, id.length - 5) + 'Modal').css('display', 'none'); }
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
