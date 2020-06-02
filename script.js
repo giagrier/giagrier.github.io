a63e1f57da8fe910b40efcdee3a5da12f94c6093
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-06-01 23:46:06 -0400

    new modals! new content! same ol gia

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 $('#myContactBtn').on('click', function() { $('#myContactModal').css('display', 'block'); });
-$('.c_close').first().on('click', function() { $('#myContactModal').css('display', 'none'); });
+$('#c_close').on('click', function() { $('#myContactModal').css('display', 'none'); });
 
 $('#myPBtn').on('click', function() { $('#myPModal').css('display', 'block'); });
-$('.p_close').first().on('click', function() { $('#myPModal').css('display', 'none'); });
+$('#p_close').on('click', function() { $('#myPModal').css('display', 'none'); });
 
 $('#myFBtn').on('click', function() { $('#myFModal').css('display', 'block'); });
-$('.f_close').first().on('click', function() { $('#myFModal').css('display', 'none'); });
+$('#f_close').on('click', function() { $('#myFModal').css('display', 'none'); });
 
 $('#myArtBtn').on('click', function() { $('#myArtModal').css('display', 'block'); });
-$('.art_close').first().on('click', function() { $('#myArtModal').css('display', 'none'); });
+$('#art_close').on('click', function() { $('#myArtModal').css('display', 'none'); });
+
+$('#myCampBtn').on('click', function() { $('#myCampModal').css('display', 'block'); });
+$('#camp_close').on('click', function() { $('#myCampModal').css('display', 'none'); });
+
+$('#myEPBtn').on('click', function() { $('#myEPModal').css('display', 'block'); });
+$('#ep_close').on('click', function() { $('#myEPModal').css('display', 'none'); });
+
+$('#mySisBtn').on('click', function() { $('#mySisModal').css('display', 'block'); });
+$('#sis_close').on('click', function() { $('#mySisModal').css('display', 'none'); });
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
@@ -17,7 +26,7 @@ $(async function() {
   var r = ['innovator', 'creator', 'friend', 'thinker', 'writer', 'citizen', 'ideator', 'GRIER'];
   var i = 0;
   await sleep(2000);
-  
+
   while (true) {
     await sleep(2000);
     $('#gia').html(l[i]);
