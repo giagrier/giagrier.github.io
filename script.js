933f1f0348908b989cb9e9b6b571967f26ad6550
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-06-03 23:05:15 -0400

    woooo events: whitney and new museum draft 1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// remove 'Btn' from end of id
 function show(id) { $('#' + id.substr(0, id.length - 3) + 'Modal').css('display', 'block'); }
+// remove 'close' from end of id
 function hide(id) { $('#' + id.substr(0, id.length - 5) + 'Modal').css('display', 'none'); }
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
@@ -17,3 +19,25 @@ $(async function() {
     i = (i + 1) % 8;
   }
 })
+
+function mus(y, h) {
+  var ht = h.substr(h.length - 1);
+  var d, x;
+  if (ht == '+') {
+    d = 'block';
+    x = ' -';
+  }
+  else if (ht == '-') {
+    d = 'none';
+    x = ' +';
+  }
+  if (y > 0) {
+    $('#_' + y).html('&nbsp; ' + y + x);
+    $('#w' + y).css('display', d);
+  }
+  else {
+    var hf = h.split(' ')[0];
+    $('#' + hf[0]).html(h.substr(0, h.length - 1) + x);
+    $('#' + hf).css('display', d);
+  }
+}
